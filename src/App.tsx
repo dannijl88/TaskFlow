298c644974208631fc3be6daa0dea6bd0322774a
Daniel Juan <dannijl88web@gmail.com>
2026-04-22 19:23:23 +0200

    feat: initial project setup with React, TypeScript and React Router

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <div>TaskFlow</div>
+  )
+}
+
+export default App
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage(){
+    return(
+        <div>
+            <h1>Página no encontrada</h1>
+        </div>
+    )
+}
