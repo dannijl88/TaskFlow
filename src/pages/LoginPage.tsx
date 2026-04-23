8b5b5b648db66305714ce36de7fb50b8f50472f7
Daniel Juan <dannijl88web@gmail.com>
2026-04-23 11:16:28 +0200

    feat: add login page with BEM styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+import { useAuth } from "../hooks/useAuth"
+import '../styles/login.css'
+
 export default function LoginPage(){
+
+    const auth = useAuth()
+    const [ username, setUsername] = useState<string>("")
+    const [ password, setPassword] = useState<string>("")
+
+    if(!auth) return null
+    const { login } = auth
+
+    const handleSubmit = () => {
+        login(username, password)
+    }
+
     return(
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <main className="login-page">
+            <form className="login-form">
+                <h1 className="login-form__title">Inicia sesión</h1>
+                <label htmlFor="username" className="login-form__label">Usuario</label>
+                <input type="text" className="login-form__input" id="username" name="username" onChange={(e) => setUsername(e.target.value)} />
+                <label htmlFor="password" className="login-form__label">Contraseña</label>
+                <input type="password" className="login-form__input" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+                <button className="login-form__button" onClick={handleSubmit}>Entrar</button>
+            </form>
+        </main>
     )
 }
