4d790cceedb2557ecdec56ec6fb1f11a93f1728d
Daniel Juan <dannijl88web@gmail.com>
2026-04-28 12:02:56 +0200

    test: add delete task functionality test

--- src/context/TaskContext.test.tsx
+++ src/context/TaskContext.test.tsx
@@ -6,12 +6,13 @@ import userEvent from "@testing-library/user-event"
 const TestComponent = () => {
     const taskContext = useTasks()
     if(!taskContext) return null
-    const { tasks, addTask } = taskContext
+    const { tasks, addTask, deleteTask } = taskContext
     
     return (
         <div>
             <span>Total: {tasks.length}</span>
             <button onClick={() => addTask('Tarea de prueba')}>Añadir</button>
+            <button onClick={() => deleteTask(tasks[0].id)}>Borrar</button>
         </div>
     )
 }
@@ -27,6 +28,25 @@ test('debe añadir una tarea al hacer click en el botón añadir', async () => {
     expect(texto).toBeInTheDocument()
     await userEvent.click(screen.getByRole('button', {name: /Añadir/i}))
     await waitFor(() => {
-    expect(screen.getByText((content, element) => element?.textContent === 'Total: 1')).toBeInTheDocument()
+        expect(screen.getByText((content, element) => element?.textContent === 'Total: 1')).toBeInTheDocument()
+    })
 })
+
+test('debe borrar la tarea al hacer click en el boton borrar', async () => {
+    render(
+        <TaskProvider>
+            <TestComponent />
+        </TaskProvider>
+    )
+
+    const texto = screen.getByText((content, element) => element?.textContent === 'Total: 0')
+    expect(texto).toBeInTheDocument()
+    await userEvent.click(screen.getByRole('button', {name: /Añadir/i}))
+    await waitFor(() => {
+        expect(screen.getByText((content, element) => element?.textContent === 'Total: 1')).toBeInTheDocument()
+    })
+    await userEvent.click(screen.getByRole('button', {name: /Borrar/i}))
+    await waitFor(() => {
+        expect(screen.getByText((content, element) => element?.textContent === 'Total: 0')).toBeInTheDocument()
+    })
 })
